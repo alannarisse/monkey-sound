87d75bed3b9a736c9a4f6eb6a14093de25353bfc
alannarisse <angrypirate@gmail.com>
2022-02-07 11:04:53 -0800

    added longer audio with play pause feature

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 $("#monkey-button").click(function () {
   $("#monkey-sound")[0].play()
 })
+$("#taylor-button").click(function () {
+  var tayAudio = $("#taytay")[0]
+  if (tayAudio.paused == true) {
+    $("#taytay")[0].play()
+    $("#taylor-button").html("Pause Taylor")
+
+    //$("audio#pop")[0].currentTime = 0
+  } else {
+    $("#taytay")[0].pause()
+    $("#taylor-button").html("Play Taylor")
+  }
+})
+
+$("#reset-button").click(function () {
+  $("#taytay")[0].currentTime = 0
+  $("#taytay")[0].play()
+})
+
+// $('audio#pop')[0].pause()
+// $('audio#pop')[0].currentTime = 0
